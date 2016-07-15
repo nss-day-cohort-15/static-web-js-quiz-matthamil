c0379bb8bd62f792fe77446038052a5fe573eead
Matt Hamil <j.matthew.hamil@gmail.com>
2016-07-15 11:41:03 -0500

    Completed static-web-js quiz

---
+++ quiz.js
@@ -0,0 +1,52 @@
+let heightInput = document.querySelector('input[name="height"]');
+let charInput = document.querySelector('input[name="character"]');
+let growTreeButton = document.querySelector('button');
+
+function tree(obj) {
+  // obj.height = number
+  // obj.char = string
+  var str = '';
+  var toAdd = 1;
+
+  console.log('🎅❄️ Here\'s your tree 🎄⛄️');
+
+  for (let i = 0; i < obj.height; i++) {
+    str = ' '.repeat(obj.height - i).concat(obj.char.repeat(toAdd));
+    toAdd += 2;
+    console.log('%c' + str, 'color: #2CA441;');
+  }
+}
+
+function growTree() {
+  if (validateInput()) {
+    let treeObject = {
+      height: heightInput.value,
+      char: charInput.value
+    }
+
+    tree(treeObject);
+  }
+}
+
+function validateInput() {
+  if (!heightInput.value || !charInput.value) {
+    alert('Both input fields need a value!');
+    return false;
+  }
+
+  return true;
+}
+
+growTreeButton.addEventListener('click', growTree);
+
+heightInput.addEventListener('keyup', function enterKey(e) {
+  if (e.keyCode == 13) {
+    growTree();
+  }
+});
+
+charInput.addEventListener('keyup', function enterKey(e) {
+  if (e.keyCode == 13) {
+    growTree();
+  }
+});
